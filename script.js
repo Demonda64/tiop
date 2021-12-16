ebc5da9fde2ede5d2ecc62f7a13776ac6193bbd0
Oscar64 <oscardemonda@outlook.fr>
2021-12-16 16:00:39 +0100

    tiop

---
+++ script.js
@@ -0,0 +1,13 @@
+var inpType = document.querySelector('.form-groupe:nth-child(1) select');
+
+var inpCouleur = document.querySelector('.form-groupe:nth-child(2) input');
+
+
+
+
+
+var voiture = {
+    type: "citadine",
+    couleur: "red",
+    
+}
